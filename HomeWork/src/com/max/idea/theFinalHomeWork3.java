c0cc9f424d9bad4f61c252303dbaa9a7573e544a
Nazar <gre.naz@ya.ru>
2021-02-27 21:40:18 +0300

    Выполенено задание theFinalHomeWork3

--- HomeWork/src/com/max/idea/theFinalHomeWork3.java
+++ HomeWork/src/com/max/idea/theFinalHomeWork3.java
@@ -1,5 +1,7 @@
 package com.max.idea;
 
+import java.util.Scanner;
+
 public class theFinalHomeWork3 {
     public static void main(String[] args) {
         /*Напишите программу, где пользователь вводит сначала количество строк n, затем сами строки.
@@ -11,5 +13,25 @@ public class theFinalHomeWork3 {
         Строка 2: анализ
         Строка 3: 111111111111
         Ответ: привет*/
+
+
+        int stringNum;
+        int index = 0;
+        int maxNumberCharInString = 0;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите количество строк:");
+        stringNum = scanner.nextInt();
+        String[] stringsArray = new String[stringNum];
+        int[] numberCharInString = new int[stringNum];
+        System.out.println("Введите строки:");
+        for (int i = 0; i < stringsArray.length; i++) {
+            stringsArray[i] = scanner.next();
+            numberCharInString[i] = (int) stringsArray[i].chars().distinct().count();
+            if (numberCharInString[i] > maxNumberCharInString){
+                maxNumberCharInString = numberCharInString[i];
+                index = i;
+            }
+        }
+        System.out.println("Ответ:" + stringsArray[index]);
     }
 }
