31cfd6abcb575e52a6c9156c764aa7494a674063
Nazar <gre.naz@ya.ru>
2021-02-27 19:00:08 +0300

    Выполенено задание theFinalHomeWork2

--- HomeWork/src/com/max/idea/theFinalHomeWork2.java
+++ HomeWork/src/com/max/idea/theFinalHomeWork2.java
@@ -1,5 +1,7 @@
 package com.max.idea;
 
+import java.util.Scanner;
+
 public class theFinalHomeWork2 {
     public static void main(String[] args) {
         /*Напишите программу, которая позволит решить простое уравнение относительно x.
@@ -14,5 +16,49 @@ public class theFinalHomeWork2 {
         Вывод: -6
         Ввод: 3-3=x
         Вывод: 0*/
+
+        int variableIndex;
+        String inputString;
+        int const1;
+        int const2;
+        System.out.println("Введите уравнение:");
+        Scanner scanner= new Scanner(System.in);
+        inputString =scanner.nextLine();
+        variableIndex = inputString.indexOf("x");
+
+        if (inputString.contains("+")){
+            if (variableIndex == 0){
+                const1 = Character.getNumericValue(inputString.charAt(2));
+                const2 = Character.getNumericValue(inputString.charAt(4));
+                System.out.println(const2 - const1);
+            }
+            else if (variableIndex == 2){
+                const1 = Character.getNumericValue(inputString.charAt(0));
+                const2 = Character.getNumericValue(inputString.charAt(4));
+                System.out.println(const2 - const1);
+            }
+            else {
+                const1 = Character.getNumericValue(inputString.charAt(0));
+                const2 = Character.getNumericValue(inputString.charAt(2));
+                System.out.println(const2 + const1);
+            }
+        }
+        else if (inputString.contains("-")){
+            if (variableIndex == 0){
+                const1 = Character.getNumericValue(inputString.charAt(2));
+                const2 = Character.getNumericValue(inputString.charAt(4));
+                System.out.println(const2 + const1);
+            }
+            else if (variableIndex == 2){
+                const1 = Character.getNumericValue(inputString.charAt(0));
+                const2 = Character.getNumericValue(inputString.charAt(4));
+                System.out.println(const2 + const1);
+            }
+            else {
+                const1 = Character.getNumericValue(inputString.charAt(0));
+                const2 = Character.getNumericValue(inputString.charAt(2));
+                System.out.println(const1 - const2);
+            }
+        }
     }
 }
